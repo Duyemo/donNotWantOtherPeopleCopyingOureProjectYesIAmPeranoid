b35a1e80052daf02989237780e26da6c41399cae
Marit <maritradder@gmail.com>
2018-12-10 11:10:23 +0100

    websockets stuff

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var websocket = require("ws");
 var port = process.argv[2];
 var app = express();
 
+// make client array
+clients = [];
+
+
 // for the routes
 app.use(express.static(__dirname + "/public"));
 app.use("/", indexRouter);
@@ -20,7 +24,8 @@ const wss = new websocket.Server({ server });
 
 // if connected, sends connected to server. And print to console log messages from server
 wss.on("connection", function(ws) {
-    
+    // push the new client to the clients array
+    clients.push(ws);
     ws.send("connected to server ");
     ws.on("message", function incoming(message) {
         console.log(message);
@@ -34,8 +39,15 @@ wss.on('connection', (ws) => {
         console.log('received: %s', m);
     });
 
-    console.log(wss.clients);
+    console.log(wss.clients.size);
+    
 });
+
+// test test test 
+if(clients.size > 2){
+    clients[0].send("you are player one");
+    clients[1].send("you are player two"); 
+}
 
 // listens to port 3000
 server.listen(port);
