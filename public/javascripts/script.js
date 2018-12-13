c98e4c0f39661bbed2b8e20379e04ca77fa6208c
Duyemo <45041539+Duyemo@users.noreply.github.com>
2018-12-13 18:18:16 +0100

    Now consol displays if a ship sunk or not

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,29 @@
 function fire(elem) {
     var shipStat = 1;
 
+    var hasSunk = 0;
+    var hasNotSunk =0; 
+    var sunkOrNot = function(elemID, previous){
+      
+        if (opponentsField[elemID + 1] == "contains"||
+        opponentsField[elemID - 1] == "contains"||
+        opponentsField[elemID + 11] == "contains"||
+        opponentsField[elemID - 11] == "contains"){
+            hasNotSunk ++;
+        }
+        else{
+            if(opponentsField[elemID+ 1 ]=="sunk" && previous != "right" &&opponentsField[elemID - 1] == "sunk" && previous != "left"){
+                sunkOrNot(elemID+1, "left");   sunkOrNot(elemID-1, "right"); hasNotSunk= hasNotSunk +2;}
+            if(opponentsField[elemID + 11] == "sunk" && previous != "above" && opponentsField[elemID - 11] == "sunk" && previous != "below"){
+                sunkOrNot(elemID+11, "below");  sunkOrNot(elemID-11, "above");  hasNotSunk= hasNotSunk +2;}
+            if(opponentsField[elemID + 1] == "sunk" && previous != "right"){sunkOrNot(elemID+1, "left");}
+            if(opponentsField[elemID - 1] == "sunk" && previous != "left"){sunkOrNot(elemID-1, "right");}
+            if(opponentsField[elemID + 11] == "sunk" && previous != "above"){sunkOrNot(elemID+11, "below");}
+            if(opponentsField[elemID - 11] == "sunk" && previous != "below"){sunkOrNot(elemID-11, "above");}
+            else{hasSunk++;}
+        }
+    }
+
     console.log(elem.id);
     console.log("id opponents field", opponentsField[12]);
     console.log(opponentsField[elem.id - 1] == "contains");
@@ -10,6 +33,14 @@ function fire(elem) {
         if (opponentsField[elem.id - 1] == "contains" && (elem.style.backgroundColor != "rgb(0, 255, 0)" && elem.style.backgroundColor != "tomato")) {
             hit();
             elem.style.backgroundColor = "#00FF00";
+            opponentsField[elem.id - 1] = "sunk";
+            sunkOrNot(elem.id-1," ");
+            if(hasSunk > hasNotSunk){console.log("ship has sunk");}
+            else{console.log("ship has NOOOOOOOT sunk");}
+            hasSunk=0; 
+            hasNotSunk=0;
+            
+            
         
         }
         if (opponentsField[elem.id - 1] != "contains" && (elem.style.backgroundColor != "tomato" && elem.style.backgroundColor != "rgb(0, 255, 0)")) {
