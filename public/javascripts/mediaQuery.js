00f613084a0323f71c80caaef17684d2655d85d9
Duyemo <45041539+Duyemo@users.noreply.github.com>
2018-12-16 15:32:27 +0100

    mediaQuerys bug fixes

--- public/javascripts/mediaQuery.js
+++ public/javascripts/mediaQuery.js
@@ -1,25 +1,23 @@
 //var testScreenSize = function (){
-var mql = window.matchMedia('(min-width: 1470px)'); //min width is absolute position of opponent table + it's width
-//var heightCheck = window.matchMedia('(min-height: 595px)');//min height is the absolute bposition of opponent table+ it's height. with this to requirmnts the game is 100%playable. although some thing will fal of screen
-//var sizeCheck= true;
+var widthCheck = window.matchMedia('(min-width: 1470px)'); //min width is absolute position of opponent table + it's width
+var heightCheck = window.matchMedia('(min-height: 595px)');//min height is the absolute bposition of opponent table+ it's height. with this to requirmnts the game is 100%playable. although some thing will fal of screen
+var sizeCheck= true;
 
 function screenTestWidth(e) {
   if (e.matches) {console.log("size is fine");}
-  else {console.log("problem");}
+  else {sizeCheck = false;}
 }
-mql.addListener(screenTestWidth);
+widthCheck.addListener(screenTestWidth);
 
-/*
 function screenTestHeight(e){
-  if(e.matches) {console.log("hight is ok");}
+  if(e.matches){console.log("hight is ok");}
   else{sizeCheck = false;}
 }
 heightCheck.addListener(screenTestHeight);
 
 
-if(sizeCheck === false){
-    window.alert("Screensize is to small!!! \nYour sceensize should be at least 1470x959px");
-}*/
+if(sizeCheck == flase){window.alert("The window size is to small!!! \n It should be at least 1470x595px");}
+
 
 //}
 //$(document).ready(testScreenSize);
